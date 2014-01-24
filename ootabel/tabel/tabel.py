ed7930261952eab3a5c0375cf79ba16170864f92
Ants Torim <ants.torim@ttu.ee>
2014-01-24 17:53:04 +0200

    i don't know what

--- ootabel/tabel/tabel.py
+++ ootabel/tabel/tabel.py
@@ -27,13 +27,36 @@ class Tabel(list):
                 result[i][row[i]] = result[i].get(row[i], 0) + 1
         return result
     
+    
     @property        
     def weight(self):
         result = {}
         for j in range(len(self)):
-            result[j] = {}
+            #result[j] = {}
+            result[j] = 0
             for i in range(len(self[j])):
                 value = self[j][i]
-                result[j][value] = result[j].get(value, 0) + self.freq[i][value] 
+                #result[j][value] = result[j].get(value, 0) + self.freq[i][value] 
+                result[j] += self.freq[i][value]
         return result
-            
+   
+   
+class KaalutudTabel(Tabel):
+    
+    
+    def __init__(self, r, c, kaalud):
+        assert(len(kaalud)==r)
+        Tabel.__init__(self, r, c)
+        self.reakaalud = kaalud
+        
+        
+             
+    @property        
+    def freq(self):
+        result = {}
+        for i in range(len(self[0])):
+            result[i] = {}
+        for row, kaal in zip(self, self.reakaalud):
+            for i in range(len(row)):
+                result[i][row[i]] = result[i].get(row[i], 0) + kaal
+        return result
--- ootabel/test/test.py
+++ ootabel/test/test.py
@@ -4,23 +4,31 @@ Created on 19.07.2013
 @author: Ants Torim
 '''
 import unittest
-from tabel import Tabel
+from tabel import Tabel, KaalutudTabel
 
 
 class Test(unittest.TestCase):
 
 
-    def testName(self):
+    def testSimple(self):
         t = Tabel(4, 5)
         t[0][0] = 1
         self.assertEquals(t[0][0], 1)
         self.assertEquals(t[0][1], 0)
         self.assertEqual(t.freq[0][0], 3)
         self.assertEqual(t.freq[0][1], 1)
-        self.assertEqual(t.weight[0][0], 16)
-        self.assertEqual(t.weight[0][1], 1)
-        self.assertEqual(t.weight[1][0], 19)
+        self.assertEqual(t.weight[0], 17)
+        self.assertEqual(t.weight[1], 19)
     
+    def testWeighted(self):
+        t = KaalutudTabel(4, 5, [2, 1, 1, 1])
+        t[0][0] = 1
+        print(t.freq)
+        self.assertEqual(t.freq[0][0], 3)
+        self.assertEqual(t.freq[0][1], 2)
+        self.assertEqual(t.weight[0], 22)
+        self.assertEqual(t.weight[3], 23)
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
